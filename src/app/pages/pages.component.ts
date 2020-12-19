e6531f85260288cc6aa06007fcecd769b8cc6ca1
v.khanmohammadi <v.khanmohammadi@navaco.ir>
2020-12-19 13:12:53 +0330

    #deleted additional module

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { MENU_ITEMS } from './pages-menu';
 
 @Component({
-  selector: 'ngx-pages',
+  selector: 'app-pages',
   styleUrls: ['pages.component.scss'],
   template: `
     <ngx-one-column-layout>
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,8 @@ import { NbMenuModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
-import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   imports: [
@@ -14,11 +13,10 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     ThemeModule,
     NbMenuModule,
     DashboardModule,
-    ECommerceModule,
-    MiscellaneousModule,
   ],
   declarations: [
     PagesComponent,
+    NotFoundComponent
   ],
 })
 export class PagesModule {
